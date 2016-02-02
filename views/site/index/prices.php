8b8bd2ac665cd1813000de43d81812a9c669fed5
igoryan <yakrosafcheg@gmail.com>
2016-02-02 12:06:43 +0100

    changes (logo/pricing)

--- views/site/index/prices.php
+++ views/site/index/prices.php
@@ -21,20 +21,6 @@
 
                 <div class="row">
 
-                    <div class="col-md-12">
-
-                        <h2 class="section-separator-title separator-align-left">
-                            <span data-shadow-text="<?= \Yii::t('site', 'index.prices.dev.title'); ?>">
-                                <?= \Yii::t('site', 'index.prices.dev.title'); ?>
-                            </span>
-                        </h2>
-
-                    </div>
-
-                </div>
-
-                <div class="row">
-
                     <div class="col-md-4">
 
                         <div class="pricing-table" data-animation-name="flipInY">
@@ -66,86 +52,6 @@
                                 <small>/<?= \Yii::t('labels', 'hour'); ?></small>
                             </h2>
                             <h5 class="name"><?= \Yii::t('site', 'index.prices.dev.title.3'); ?></h5>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class="row">
-
-                    <div class="col-md-12">
-
-                        <h2 class="section-separator-title separator-align-left">
-                            <span data-shadow-text="<?= \Yii::t('site', 'index.prices.hosting.title'); ?>"><?= \Yii::t('site', 'index.prices.hosting.title'); ?></span>
-                        </h2>
-
-                    </div>
-
-                </div>
-
-                <div class="row">
-
-                    <div class="col-md-4">
-
-                        <div class="pricing-table" data-animation-name="flipInY">
-                            <h2 class="price">
-                                $5<small>/mo.</small>
-                            </h2>
-                            <h5 class="name">Start</h5>
-                            <div class="features">
-                                <ul>
-                                    <li><span>512MB</span> Memory</li>
-                                    <li><span>1 Core</span> processor</li>
-                                    <li><span>20GB</span> SSD Disk</li>
-                                    <li><span>1TB</span> Transfer</li>
-                                </ul>
-                            </div>
-                            <a href="https://m.do.co/c/ada07fe8e9c5" class="btn btn-sm btn-primary btn-block">Sign Up</a>
-                        </div>
-
-                    </div>
-
-                    <div class="col-md-4">
-
-                        <div class="pricing-table featured" data-animation-name="flipInY">
-                            <h2 class="price">
-                                $10<small>/mo.</small>
-                            </h2>
-                            <h5 class="name">Basic</h5>
-                            <div class="features">
-                                <ul>
-                                    <li><span>1GB</span> Memory</li>
-                                    <li><span>1 Core</span> processor</li>
-                                    <li><span>30GB</span> SSD Disk</li>
-                                    <li><span>2TB</span> Transfer</li>
-                                </ul>
-                            </div>
-                            <a href="https://m.do.co/c/ada07fe8e9c5" class="btn btn-sm btn-primary btn-block">
-                                <?= \Yii::t('labels', 'Sign up'); ?>
-                            </a>
-                        </div>
-
-                    </div>
-
-                    <div class="col-md-4">
-
-                        <div class="pricing-table" data-animation-name="flipInY">
-                            <h2 class="price">
-                                $40<small>/mo.</small>
-                            </h2>
-                            <h5 class="name">Pro</h5>
-                            <div class="features">
-                                <ul>
-                                    <li><span>4GB</span> Memory</li>
-                                    <li><span>2 Core</span> processor</li>
-                                    <li><span>60GB</span> SSD Disk</li>
-                                    <li><span>4TB</span> Transfer</li>
-                                </ul>
-                            </div>
-                            <a href="https://m.do.co/c/ada07fe8e9c5" class="btn btn-sm btn-primary btn-block">
-                                <?= \Yii::t('labels', 'Sign up'); ?>
-                            </a>
                         </div>
 
                     </div>
